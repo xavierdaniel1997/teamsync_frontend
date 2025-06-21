cbaa7067dbfdb949476a1a3fa958772c1fd41678
SAY MY NAME...!!! <danielcx532@gmail.comgit confing --global user.name daniel>
2025-06-21 11:32:53 +0530

    update the api url with new live link

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -3,7 +3,8 @@ import { logout, setCredentials } from '../redux/authSlice';
 import store from '../redux/store';
 
 
-const API_URL = 'http://localhost:5000/api/';
+// const API_URL = 'http://localhost:5000/api/';
+const API_URL = "https://api.teamsync.buzz/api/"
 
 const api = axios.create({
     baseURL: API_URL,
