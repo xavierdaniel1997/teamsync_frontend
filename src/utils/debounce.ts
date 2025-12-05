dffecf6e6f186d38b838d2b9068d70fe37e96def
xavierdaniel1997 <danielcx532@gmail.com>
2025-12-05 20:20:09 +0530

    update the debounce function

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,17 +1,34 @@
 
 
-export const debounce = <T extends (...args: any[]) => void>(
-    func: T,
-    wait: number
-  ): ((...args: Parameters<T>) => void) => {
-    let timeout: NodeJS.Timeout | null = null;
+// export const debounce = <T extends (...args: any[]) => void>(
+//     func: T,
+//     wait: number
+//   ): ((...args: Parameters<T>) => void) => {
+//     let timeout: NodeJS.Timeout | null = null;
   
-    return (...args: Parameters<T>) => {
-      if (timeout) {
-        clearTimeout(timeout);
-      }
-      timeout = setTimeout(() => {
-        func(...args);
-      }, wait);
-    };
+//     return (...args: Parameters<T>) => {
+//       if (timeout) {
+//         clearTimeout(timeout);
+//       }
+//       timeout = setTimeout(() => {
+//         func(...args);
+//       }, wait);
+//     };
+//   };
+
+  export const debounce = <T extends (...args: any[]) => void>(
+  func: T,
+  wait: number
+) => {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  return (...args: Parameters<T>) => {
+    if (timeout !== null) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(() => {
+      func(...args);
+    }, wait);
   };
+};
